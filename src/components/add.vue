e7cf6376b14923d1d503fa2c017f4d405952da6a
linminghui <linminghui@gaofen.com>
2018-04-23 22:18:37 +0800

    加1

---
+++ src/components/add.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="loading">
+    <div class="loading-fixed" >
+     
+      <p>加载中...</p>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+ 
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../assets/stylus/_loading.styl"
+</style>
